16113ef49b81e9229f884105cdf24057e0b72f59
Ajay <mailtoajay7007@gmail.com>
2025-01-09 23:23:52 +0530

    ui cards

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Header from '../other/Header'
 import TaskListNumbers from '../other/TaskListNumbers'
+import TaskList from '../TaskList/TaskList'
 
 const EmployeeDashboard = () => {
   return (
     <>
-        <div className='w-full '>
+        <div className='w-full h-full p-8 bg-gray-800 '>
 
             <Header />
             <TaskListNumbers />
+            <TaskList/>
         </div>
 
     </>
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const TaskList = () => {
   return (
-    <div>
+    <div className='h-[55%] w-full bg-red-500 py-5 flex justify-start items-center flex-nowrap mt-10'>
+    <h1 className='h-full text-white text-4xl font-thin flex justify-center items-center w-[300px] bg-yellow-500 rounded-xl mx-auto'>
+
+    TaskList
+    </h1>
+    <h1 className='h-full text-white text-4xl font-thin flex justify-center items-center w-[300px] bg-yellow-500 rounded-xl mx-auto'>
+
+    TaskList
+    </h1>
+    <h1 className='h-full text-white text-4xl font-thin flex justify-center items-center w-[300px] bg-yellow-500 rounded-xl mx-auto'>
+
+    TaskList
+    </h1>
+    <h1 className='h-full text-white text-4xl font-thin flex justify-center items-center w-[300px] bg-yellow-500 rounded-xl mx-auto'>
+
+    TaskList
+    </h1>
       
     </div>
   )
