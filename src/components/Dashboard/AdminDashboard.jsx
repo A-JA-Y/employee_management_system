f79a3414a38f5ae220837b216f2241651cf56969
Ajay <mailtoajay7007@gmail.com>
2025-01-12 10:32:54 +0530

    adjstmnt

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -5,7 +5,7 @@ import AllTask from '../other/AllTask'
 
 const AdminDashboard = () => {
   return (
-    <div className="min-h-screen w-full px-4 py-8 bg-gray-900 text-zinc-300">
+    <div className="min-h-screen w-full px-4 py-8  text-zinc-300">
       <Header />
       <div className="grid w-full max-w-7xl mx-auto grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-8">
         <CreateTask />
