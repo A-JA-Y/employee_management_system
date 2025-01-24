94e77a9bc180f872f868058045a887be3e5bea81
Ajay <mailtoajay7007@gmail.com>
2025-01-24 22:47:19 +0530

    unnecessary changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import Login from "./components/Auth/Login";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 
+
 function App() {
   return (
     <>
       <div className="dark:bg-zinc-900 dark:text-white w-full h-[1000px] m-0">
-        {/* <Login /> */}
-        {/* <EmployeeDashboard /> */}
+        {/* <Login />
+        <EmployeeDashboard /> */}
         <AdminDashboard />
+        
       </div>
     </>
   );
