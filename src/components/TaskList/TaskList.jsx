e9cab41d8f11cf89b1240aaa025710de7ed5b961
Ajay <mailtoajay7007@gmail.com>
2025-01-31 23:38:41 +0530

    Tasklist responsive

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -4,12 +4,12 @@ const TaskList = () => {
   return (
     <div
       id="tasklist"
-      className="h-[55%] w-full  gap-5 overflow-x-auto  flex justify-start items-center flex-nowrap mt-10"
+      className="w-full p-2 mt-10 flex flex-wrap md:flex-nowrap gap-5 md:gap-8 overflow-x-auto md:h-[55%]"
     >
-      <div className="h-full flex-shrink-0  text-white shadow-inner  shadow-red-800  font-thin p-5  w-[300px] bg-gray-500 rounded-xl mx-auto">
-        <div className="flex  justify-between items-center">
-          <h3 className=" text-3xl  drop-shadow-xl  rounded-lg">High</h3>
-          <h4 className="text-2xl ">20 feb 2025</h4>
+      <div className="w-full sm:w-[300px] flex-shrink-0 bg-gray-500 rounded-xl mx-auto mb-5 text-white shadow-inner shadow-red-800 font-thin p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="text-3xl drop-shadow-xl rounded-lg">High</h3>
+          <h4 className="text-2xl">20 feb 2025</h4>
         </div>
         <h2 className="font-semibold text-2xl mt-2">Make video</h2>
         <p className="text-lg mt-2">
@@ -17,10 +17,10 @@ const TaskList = () => {
           malesuada, odio eget tincidunt.
         </p>
       </div>
-      <div className="h-full flex-shrink-0  text-white shadow-inner  shadow-red-800  font-thin p-5  w-[300px] bg-green-500 rounded-xl mx-auto">
-        <div className="flex  justify-between items-center">
-          <h3 className=" text-3xl  drop-shadow-xl  rounded-lg">High</h3>
-          <h4 className="text-2xl ">20 feb 2025</h4>
+      <div className="w-full sm:w-[300px] flex-shrink-0 bg-green-500 rounded-xl mx-auto mb-5 text-white shadow-inner shadow-red-800 font-thin p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="text-3xl drop-shadow-xl rounded-lg">High</h3>
+          <h4 className="text-2xl">20 feb 2025</h4>
         </div>
         <h2 className="font-semibold text-2xl mt-2">Make video</h2>
         <p className="text-lg mt-2">
@@ -28,10 +28,10 @@ const TaskList = () => {
           malesuada, odio eget tincidunt.
         </p>
       </div>
-      <div className="h-full flex-shrink-0  text-white shadow-inner  shadow-red-800  font-thin p-5  w-[300px] bg-yellow-500 rounded-xl mx-auto">
-        <div className="flex  justify-between items-center">
-          <h3 className=" text-3xl  drop-shadow-xl  rounded-lg">High</h3>
-          <h4 className="text-2xl ">20 feb 2025</h4>
+      <div className="w-full sm:w-[300px] flex-shrink-0 bg-yellow-500 rounded-xl mx-auto mb-5 text-white shadow-inner shadow-red-800 font-thin p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="text-3xl drop-shadow-xl rounded-lg">High</h3>
+          <h4 className="text-2xl">20 feb 2025</h4>
         </div>
         <h2 className="font-semibold text-2xl mt-2">Make video</h2>
         <p className="text-lg mt-2">
@@ -39,10 +39,10 @@ const TaskList = () => {
           malesuada, odio eget tincidunt.
         </p>
       </div>
-      <div className="h-full flex-shrink-0  text-white shadow-inner  shadow-red-800  font-thin p-5  w-[300px] bg-blue-500 rounded-xl mx-auto">
-        <div className="flex  justify-between items-center">
-          <h3 className=" text-3xl  drop-shadow-xl  rounded-lg">High</h3>
-          <h4 className="text-2xl ">20 feb 2025</h4>
+      <div className="w-full sm:w-[300px] flex-shrink-0 bg-blue-500 rounded-xl mx-auto mb-5 text-white shadow-inner shadow-red-800 font-thin p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="text-3xl drop-shadow-xl rounded-lg">High</h3>
+          <h4 className="text-2xl">20 feb 2025</h4>
         </div>
         <h2 className="font-semibold text-2xl mt-2">Make video</h2>
         <p className="text-lg mt-2">
@@ -50,10 +50,10 @@ const TaskList = () => {
           malesuada, odio eget tincidunt.
         </p>
       </div>
-      <div className="h-full flex-shrink-0  text-white shadow-inner  shadow-red-800  font-thin p-5  w-[300px] bg-gray-500 rounded-xl mx-auto">
-        <div className="flex  justify-between items-center">
-          <h3 className=" text-3xl  drop-shadow-xl  rounded-lg">High</h3>
-          <h4 className="text-2xl ">20 feb 2025</h4>
+      <div className="w-full sm:w-[300px] flex-shrink-0 bg-gray-500 rounded-xl mx-auto mb-5 text-white shadow-inner shadow-red-800 font-thin p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="text-3xl drop-shadow-xl rounded-lg">High</h3>
+          <h4 className="text-2xl">20 feb 2025</h4>
         </div>
         <h2 className="font-semibold text-2xl mt-2">Make video</h2>
         <p className="text-lg mt-2">
@@ -61,10 +61,10 @@ const TaskList = () => {
           malesuada, odio eget tincidunt.
         </p>
       </div>
-      <div className="h-full flex-shrink-0  text-white shadow-inner  shadow-red-800  font-thin p-5  w-[300px] bg-green-500 rounded-xl mx-auto">
-        <div className="flex  justify-between items-center">
-          <h3 className=" text-3xl  drop-shadow-xl  rounded-lg">High</h3>
-          <h4 className="text-2xl ">20 feb 2025</h4>
+      <div className="w-full sm:w-[300px] flex-shrink-0 bg-green-500 rounded-xl mx-auto mb-5 text-white shadow-inner shadow-red-800 font-thin p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="text-3xl drop-shadow-xl rounded-lg">High</h3>
+          <h4 className="text-2xl">20 feb 2025</h4>
         </div>
         <h2 className="font-semibold text-2xl mt-2">Make video</h2>
         <p className="text-lg mt-2">
@@ -72,10 +72,10 @@ const TaskList = () => {
           malesuada, odio eget tincidunt.
         </p>
       </div>
-      <div className="h-full flex-shrink-0  text-white shadow-inner  shadow-red-800  font-thin p-5  w-[300px] bg-yellow-500 rounded-xl mx-auto">
-        <div className="flex  justify-between items-center">
-          <h3 className=" text-3xl  drop-shadow-xl  rounded-lg">High</h3>
-          <h4 className="text-2xl ">20 feb 2025</h4>
+      <div className="w-full sm:w-[300px] flex-shrink-0 bg-yellow-500 rounded-xl mx-auto mb-5 text-white shadow-inner shadow-red-800 font-thin p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="text-3xl drop-shadow-xl rounded-lg">High</h3>
+          <h4 className="text-2xl">20 feb 2025</h4>
         </div>
         <h2 className="font-semibold text-2xl mt-2">Make video</h2>
         <p className="text-lg mt-2">
@@ -83,10 +83,10 @@ const TaskList = () => {
           malesuada, odio eget tincidunt.
         </p>
       </div>
-      <div className="h-full flex-shrink-0  text-white shadow-inner  shadow-red-800  font-thin p-5  w-[300px] bg-blue-500 rounded-xl mx-auto">
-        <div className="flex  justify-between items-center">
-          <h3 className=" text-3xl  drop-shadow-xl  rounded-lg">High</h3>
-          <h4 className="text-2xl ">20 feb 2025</h4>
+      <div className="w-full sm:w-[300px] flex-shrink-0 bg-blue-500 rounded-xl mx-auto mb-5 text-white shadow-inner shadow-red-800 font-thin p-5">
+        <div className="flex justify-between items-center">
+          <h3 className="text-3xl drop-shadow-xl rounded-lg">High</h3>
+          <h4 className="text-2xl">20 feb 2025</h4>
         </div>
         <h2 className="font-semibold text-2xl mt-2">Make video</h2>
         <p className="text-lg mt-2">
